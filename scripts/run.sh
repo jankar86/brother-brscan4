dba20f14f6ab08029b3397528ac20e5e25b5521d
Paul Bailey <psbailey86@gmail.com>
2020-08-11 14:03:11 -0500

    add run file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
-docker run -it -v -name scan /home/$USER/scans:/scans -e "NAME=Scanner" -e "MODEL=MFC-L2700DW" -e "IPADDRESS=192.168.1.207" --net=host jankar/brscan-skey:latest
+docker run -it --name=scan -v /home/$USER/scans:/scans -e "NAME=Scanner" -e "MODEL=MFC-L2700DW" -e "IPADDRESS=192.168.1.207" --net=host jankar/brscan-skey:latest
 
-docker run -d -v -name scan /home/$USER/scans:/scans -e "NAME=Scanner" -e "MODEL=MFC-L2700DW" -e "IPADDRESS=192.168.1.207" --net=host jankar/brscan-skey:latest
+#docker run -d -name scan -v /home/$USER/scans:/scans -e "NAME=Scanner" -e "MODEL=MFC-L2700DW" -e "IPADDRESS=192.168.1.207" --net=host jankar/brscan-skey:latest
