03ca71541a0371853123f250fc349a3e511f6f47
Jankar <psbailey86@gmail.com>
2024-04-21 09:28:23 -0500

    Updae to include container version

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 echo "Starting brscan-skey......."
 sleep 5
 echo "using the following environment variables:"
-
+echo "Container Version: " $VERSION
 echo
 #/usr/bin/brsaneconfig4 -a name=$SCANNER_NAME model=$SCANNER_MODEL ip=$SCANNER_IP_ADDRESS
 /usr/bin/brscan-skey
